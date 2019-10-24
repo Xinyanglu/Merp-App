944b074ee6550bbca7c330088d9a8de79b80840e
Lucas Liu <lucasliu02@gmail.com>
2019-10-24 14:33:12 -0400

    joe

--- app/src/main/java/com/merp/android/Database.kt
+++ app/src/main/java/com/merp/android/Database.kt
@@ -3,7 +3,7 @@ package com.merp.android
 class Database {
     val date = ArrayList<Int>()
     fun addExpense(){
-        val d:String
-        val c:String
+        val d: String
+        val c: String
     }
 }
