4b96667d886b5ebd01f79c66cf9db66948064ff2
Lucas Liu <lucasliu02@gmail.com>
2019-10-25 12:32:56 -0400

    main activity

---
+++ app/src/main/java/com/merp/android/TempViewExpenses.kt
@@ -0,0 +1,13 @@
+package com.merp.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TempViewExpenses : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temp_view_expenses)
+
+    }
+}
