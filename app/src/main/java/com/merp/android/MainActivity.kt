df8b3184d6688a0881466b266977007fcb74666f
Lucas Liu <lucasliu02@gmail.com>
2019-10-26 10:54:15 -0400

    Get and display device date functionality added to main activity

--- app/src/main/java/com/merp/android/MainActivity.kt
+++ app/src/main/java/com/merp/android/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat;
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainDate.text = getDate().format(Date())
         btnEarnings.setOnClickListener {
             startActivity(Intent(this, TempViewExpenses::class.java))
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        mainDate.text = getDate().format(Date())
+    }
+
+    /**
+     * Gets current date in format: October 15, 2019
+     */
+    fun getDate(): SimpleDateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.CANADA)
 }
