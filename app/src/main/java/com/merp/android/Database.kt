c41fc0e32e40158a8c55af36ee29c6ea7acc24aa
Yang Lu <xinyanglu664@gmail.com>
2019-11-06 17:57:15 -0500

    Added search function

--- app/src/main/java/com/merp/android/Database.kt
+++ app/src/main/java/com/merp/android/Database.kt
@@ -1,16 +1,38 @@
 package com.merp.android
 
 import java.math.BigDecimal
+import com.merp.android.Date as Date
 
 object Database {
     val expense = ArrayList<Expense>()
     val earning = ArrayList<Earning>()
 
     fun addExpense(date: Date, category: String, price: BigDecimal, description: String, adi: String){
+
         expense.add(Expense(date,category,price,description,adi))
     }
 
-    fun addEarning(date: Date, source: String, amount: BigDecimal, adi: String){
-        earning.add(Earning(date,source,amount,adi))
+    fun addEarning(date: Date, source: String, amount: BigDecimal, adi: String) {
+        earning.add(Earning(date, source, amount, adi))
+    }
+
+    //returns the index value of the array to include in the range
+    fun search(date: Date, list: ArrayList<Date>, min: Int, max: Int): Int{
+        var i = (max+min)/2
+        var d = list[i]
+        if (max-min==1) {
+            return if (d.compareTo(date) == 1) i + 1
+            else i
+        }
+
+        return when (d.compareTo(date)) {
+            1 ->  search(date, list, i, max)
+            -1 ->  search(date, list, min, i)
+            else -> i
+        }
+    }
+
+    fun searchDateRange(start: Date, end: Date, list: ArrayList<Date>): MutableList<Date>{
+        return list.subList(search(start,list,0,list.size-1),search(end,list,0,list.size-1)+1)
     }
 }
--- app/src/main/java/com/merp/android/Date.kt
+++ app/src/main/java/com/merp/android/Date.kt
@@ -2,35 +2,55 @@ package com.merp.android
 
 import java.time.*
 
-class Date constructor(month: Int, day: Int, year: Int){
+class Date constructor(month: Int, day: Int, year: Int) : Comparable<Date>{
+
+    //Compares the dates and returns 1 which means larger, -1 means less, 0 means equal
+    override fun compareTo(other: Date): Int{
+        if (this.getYear()>other.getYear()) return 1
+        if (this.getYear()<other.getYear()) return -1
+        if (this.getMonth()<other.getMonth()) return -1
+        if (this.getMonth()>other.getMonth())return 1
+        if (this.getDay()>other.getDay()) return 1
+        if (this.getDay()<other.getDay()) return -1
+        return 0
+    }
+
+    //If month is less than 10, add a 0 in front of the string of it
     private var m = if (month<10){
         "0${month}"
     }else {
         month.toString()
     }
 
+    //add 0 in front of day if less than 10
     private var d = if (day<10){
         "0${day}"
     }else{
         day.toString()
     }
 
+    //stores the year string
     private var y = year.toString()
 
+    //stores the date string
     private var date = LocalDate.parse("${y}-${m}-${d}")
 
+    //returns the value of the month (Ex. January returns 1)
     fun getMonth(): Int{
         return(date.monthValue)
     }
 
+    //returns Day of the date
     fun getDay(): Int{
         return(date.dayOfMonth)
     }
 
+    //returns year of the date
     fun getYear(): Int{
         return(date.year)
     }
 
+    //returns the entire date string
     fun getFullDate(): String{
         return date.toString()
     }
