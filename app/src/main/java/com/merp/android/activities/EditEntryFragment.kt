ef5cb807c35902121e1369ef19cb3e60357b8084
Lucas Liu <lucasliu02@gmail.com>
2019-11-28 21:19:34 -0500

    layoutAddSource now goes invisible and clears its text after adding a new source. Started creating a dim background feature for when layoutAddSource pops up

--- app/src/main/java/com/merp/android/activities/EditEntryFragment.kt
+++ app/src/main/java/com/merp/android/activities/EditEntryFragment.kt
@@ -6,15 +6,28 @@ import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.merp.android.R
+import kotlinx.android.synthetic.main.fragment_edit_entry.*
 
 class EditEntryFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_entry, container, false)
+        val v = inflater.inflate(R.layout.fragment_edit_entry, container, false)
+        val layout = v.findViewById<FrameLayout>(R.id.editEntryFragmentLayout)
+        layout.foreground.alpha = 0
+        return v
+    }
+
+    fun dimForeground(dim: Boolean) {
+        //I have no idea what I'm doing here c:
+        val viewGroup: ViewGroup? = view?.rootView?.findViewById(android.R.id.content)
+        LayoutInflater.from(context).inflate(R.layout.fragment_edit_entry, viewGroup, false)
+
+        if (dim) editEntryFragmentLayout.foreground.alpha = 200 //dim
+        else editEntryFragmentLayout.foreground.alpha = 0 //un-dim
     }
 }
