c50e2617f3be2d56ebc50de4b0b9b1d6efb95d37
Yang Lu <xinyanglu664@gmail.com>
2020-01-21 12:56:27 -0500

    Commented database class

--- app/src/main/java/com/merp/android/Database.kt
+++ app/src/main/java/com/merp/android/Database.kt
@@ -31,47 +31,47 @@ object Database {
 
 
     /**
-    Returns the earning array list
-
-    Return:
-    earning: ArrayList<Earning>
-     **/
+    * Returns the earning array list
+    *
+    * @return [earnings] ArrayList<Earning>
+     */
 
     fun getEarnings(): ArrayList<Earning> {
         return earnings
     }
 
     /**
-    Returns the expense array list
-
-    Return:
-    expense: ArrayList<Expense>
-     **/
+    * Returns the expense array list
+    *
+    * @return [expenses] ArrayList<Expense>
+     */
     fun getExpenses(): ArrayList<Expense> {
         return expenses
     }
 
     /**
-    Returns the earnings sources array list
-
-    Return:
-    earningsSources: ArrayList<String>
-     **/
+    * Returns the earnings sources array list
+    *
+    * @return: [earningsSources] ArrayList<String>
+     */
     fun getEarningsSources(): ArrayList<String> {
         return earningsSources
     }
 
     /**
-    Returns the expense sources array list
-
-    Return:
-    expensesSources: ArrayList<String>
-     **/
+    *Returns the expense sources array list
+    *
+    * @return: [expensesSources] ArrayList<String>
+     */
     fun getExpensesSources(): ArrayList<String> {
         return expensesSources
     }
 
-    //Writes all expenses from expense array list into expenses_file.
+    /**
+     * Writes all expenses from expense array list into expenses_file.
+     *
+     */
+
     fun writeExpenses() {
         val w = BufferedWriter(FileWriter(expenses_file, false))
         w.use { out ->
@@ -82,7 +82,11 @@ object Database {
         w.close()
     }
 
-    //Writes all earnings from earnings array list into earnings_file.
+    /**
+     *  Writes all earnings from earnings array list into earnings_file.
+     *
+     */
+
     fun writeEarnings() {
         val w = BufferedWriter(FileWriter(earnings_file, false))
         w.use { out ->
@@ -93,7 +97,9 @@ object Database {
         w.close()
     }
 
-    //Writes the expenses sources into expenses sources file.
+    /**
+     * Writes the expenses sources into expenses sources file.
+     */
     fun writeExpensesSources() {
         val w = BufferedWriter(FileWriter(expenses_sources_file, false))
         w.use {
@@ -103,7 +109,9 @@ object Database {
         }
     }
 
-    //Writes the earnings sources into earnings sources file.
+    /**
+     * Writes the earnings sources into earnings sources file.
+     */
     fun writeEarningsSources() {
         val w = BufferedWriter(FileWriter(earnings_sources_file, false))
         w.use {
@@ -113,7 +121,15 @@ object Database {
         }
     }
 
-    //Adds expense to expenses arraylist
+    /**
+     * Adds expense to expenses arraylist
+     *
+     * @param [date] date of the expense
+     * @param [category] category of the expense
+     * @param [price] price of the expense
+     * @param [adi] additional information
+     *
+     */
     fun addExpense(date: Date, category: String, price: BigDecimal, adi: String) {
         expenses.add(
             searchExpenses(date, expenses, 0, expenses.size - 1),
@@ -122,8 +138,16 @@ object Database {
         writeExpenses()
     }
 
-    //Adds the earning into the earnings array list and
-    // opens the earnings text file and writes everything inside the arraylist into it.
+    /**
+     * Adds earning to earnings arraylist
+     *
+     * @param [date] date of the earning
+     * @param [source] source of the earning
+     * @param [amount] amount of the earning
+     * @param [adi] additional information
+     *
+     */
+
     fun addEarning(date: Date, source: String, amount: BigDecimal, adi: String) {
         earnings.add(
             searchEarnings(date, earnings, 0, earnings.size - 1),
@@ -132,26 +156,42 @@ object Database {
         writeEarnings()
     }
 
-    //Adds the expense source to the arraylist and opens
-    // expenses source text file and writes everything inside expenses sources file into it.
+    /**
+     * Adds expense source to expense sources arraylist
+     *
+     * @param [source] source of the expense
+     *
+     */
+
     fun addExpensesSource(source: String) {
         expensesSources.add(searchExpensesSources(source, 0, expensesSources.size - 1), source)
         writeExpensesSources()
     }
 
-    //opens earnings sources text file and writes
-    // everything in the earnings source arraylist into it.
+    /**
+     * Adds earning source to earning sources arraylist
+     *
+     * @param [source] source of the earning
+     *
+     */
+
     fun addEarningsSource(source: String) {
         earningsSources.add(searchEarningsSources(source, 0, earningsSources.size - 1), source)
         writeEarningsSources()
     }
 
-    /**Searches through earnings array list and returns the index of where the date should be inserted in to preserve order
+    /**
+     * Searches through earnings array list and returns the index of where the date should be inserted in to preserve order
+     *
      * @param [date] date to look for
      * @param [list] list to search in
      * @param [min] minimum index of list to search for
      * @param [max] maximum index of list to search for
+     *
+     * @return average index of max and min
+     *
      */
+
     private fun searchEarnings(date: Date, list: ArrayList<Earning>, min: Int, max: Int): Int {
         if (earnings.isEmpty()) return 0
         if (date.compareTo(list[max].getDate()) == 1) {
@@ -171,10 +211,18 @@ object Database {
         }
     }
 
-    /**Searches through the expenses array list and returns the index of the array of where the date should go to preserve order
-     * Returns:
-     * i: Int
+    /**
+     * Searches through the expenses array list and returns the index of the array of where the date should go to preserve order
+     *
+     * @param [date] date to look for
+     * @param [list] list to search in
+     * @param [min] minimum index of list to search for
+     * @param [max] maximum index of list to search for
+     *
+     * @return average index of max and min
+     *
      */
+
     private fun searchExpenses(date: Date, list: ArrayList<Expense>, min: Int, max: Int): Int {
         if (expenses.isEmpty()) return 0
         if (date.compareTo(list[max].getDate()) == 1) {
@@ -194,10 +242,17 @@ object Database {
         }
     }
 
-    /**Searches through the expenses sources to put the source in alphabetically to preserve order
-     * Returns:
-     * i: Int
+    /**
+     * Searches through the expenses sources to put the source in alphabetically to preserve order
+     *
+     * @param [source] source to search for in list
+     * @param [min] minimum index to search in
+     * @param [max] maximum index to search in
+     *
+     * @return average index of max and min
+     *
      */
+
     private fun searchExpensesSources(source: String, min: Int, max: Int): Int {
         if (expensesSources.isEmpty()) return 0
         if (source.compareTo(expensesSources[max]) >= 1) {
@@ -221,9 +276,15 @@ object Database {
 
     /**
      * Searches through earnings sources to put the source in alphabetically to preserve the order.
-     * Returns:
-     * i: Int
+     *
+     * @param [source] source to search for
+     * @param [min] minimum index to search in
+     * @param [max] maximum index to search in
+     *
+     * @return Average index of max and min.
+     *
      */
+
     private fun searchEarningsSources(source: String, min: Int, max: Int): Int {
         if (earningsSources.isEmpty()) return 0
         if (source.compareTo(earningsSources[max]) >= 1) {
@@ -245,7 +306,10 @@ object Database {
         }
     }
 
-    //Open expenses text file to read each line and add each line of information to the expense array list.
+    /**
+     * Initializes expenses arraylist
+     */
+
     fun readExpenses() {
         expenses.clear()
         val f = BufferedReader(FileReader(expenses_file))
@@ -267,7 +331,10 @@ object Database {
         f.close()
     }
 
-    //Open earnings text file to read each line and add the information to the earning array list.
+    /**
+     * Initializes earnings ayyalist
+     */
+
     fun readEarnings() {
         earnings.clear()
         val r = BufferedReader(FileReader(earnings_file))
@@ -289,7 +356,10 @@ object Database {
         r.close()
     }
 
-    //Reads the expenses sources in the file.
+    /**
+     * Initializes expenses sources arraylist
+     */
+
     fun readExpensesSources() {
         expensesSources.clear()
         val r = BufferedReader(FileReader((expenses_sources_file)))
@@ -298,7 +368,10 @@ object Database {
         }
     }
 
-    //Reads the earnings sources in the file.
+    /**
+     * Initializes earnings sources arraylist.
+     */
+
     fun readEarningsSources() {
         earningsSources.clear()
         val r = BufferedReader(FileReader(earnings_sources_file))
@@ -306,6 +379,15 @@ object Database {
             earningsSources.add(it)
         }
     }
+
+    /**
+     * Returns a list of amount earned per category.
+     *
+     * @param [array] list of earning objects to search through.
+     *
+     * @return list of amount earned per category.
+     *
+     */
 
     fun getEarningAmountPerCategory(array: MutableList<Earning>): Array<BigDecimal> {
         val amounts = Array(getEarningsSources().size, { BigDecimal(0) })
@@ -319,6 +401,14 @@ object Database {
         return amounts
     }
 
+    /**
+     * Returns a list of amount spent per category
+     *
+     * @param [array] list of expense objects to search through
+     *
+     * @return list of amount spent per category.
+     *
+     */
     fun getExpenseAmountPerCategory(array: MutableList<Expense>): Array<BigDecimal> {
         val amounts = Array(getExpensesSources().size, { BigDecimal(0) })
         for (source in 0 until getExpensesSources().size) {
@@ -337,7 +427,10 @@ object Database {
      * @param [start] start date
      * @param [end] end date
      *
+     * @return List of earnings with dates between start and end.
+     *
      */
+
     fun searchRangeEarnings(start: Date, end: Date): MutableList<Earning> {
         var a = mutableListOf<Earning>()
         val startIndex = searchEarnings(start, earnings, 0, earnings.size - 1)
@@ -357,7 +450,10 @@ object Database {
      * @param [start] start date
      * @param [end] end date
      *
+     * @return List of expenses with dates between start and end.
+     *
      */
+
     fun searchRangeExpenses(start: Date, end: Date): MutableList<Expense> {
 
         var a = mutableListOf<Expense>()
@@ -372,6 +468,13 @@ object Database {
         return a
     }
 
+    /**
+     * Returns the earning dates within a range into string
+     *
+     * @param [array] list of earnings within within specific dates
+     *
+     * @return list of dates in string within specific list of earnings within dates
+     */
     fun getEarningDateStrings(array: MutableList<Earning>): ArrayList<String> {
         val dates = ArrayList<String>()
 
@@ -381,6 +484,14 @@ object Database {
         return dates
     }
 
+    /**
+     * Returns earning dates objects within a range of earnings
+     *
+     * @param [array] list of earning objects within a specific date
+     *
+     * @return list of dates within the list of earnings within specific date
+     *
+     */
     fun getEarningDates(array: MutableList<Earning>): ArrayList<Date> {
         var contains = false
         val dates = ArrayList<Date>()
@@ -399,6 +510,13 @@ object Database {
         return dates
     }
 
+    /**
+     * Returns string of dates within specific dates
+     *
+     * @param [array] List of expenses within specific range
+     *
+     * @return date in string within list of expenses with specific range
+     */
     fun getExpenseDateStrings(array: MutableList<Expense>): ArrayList<String> {
         val dates = ArrayList<String>()
 
@@ -408,6 +526,14 @@ object Database {
         return dates
     }
 
+    /**
+     * Returns list of date objects within specific range of expense objects
+     *
+     * @param [array] list of expenses within specific range
+     *
+     * @return list of date objects within specific range of expense objects
+     *
+     */
     fun getExpenseDates(array: MutableList<Expense>): ArrayList<Date> {
         var contains = false
         val dates = ArrayList<Date>()
@@ -426,6 +552,14 @@ object Database {
         return dates
     }
 
+    /**
+     * Returns amount earned per day
+     *
+     * @param [array] list of earning objects within specific range of dates
+     *
+     * @return amount earned per day within specific range
+     *
+     */
     fun getAmountEarnedPerDate(array: MutableList<Earning>): Array<Float> {
         val dates = getEarningDates(array)
         val amounts = Array<Float>(dates.size, { 0.toFloat() })
@@ -439,6 +573,15 @@ object Database {
         return amounts
     }
 
+    /**
+     *
+     * Returns amount spent per day
+     *
+     * @param [array] List of expense objects within specific dates
+     *
+     * @return Amount spent per date within specified range
+     *
+     */
     fun getAmountSpentPerDate(array: MutableList<Expense>): Array<Float> {
         val dates = getExpenseDates(array)
         val amounts = Array<Float>(dates.size, { 0.toFloat() })
@@ -452,7 +595,14 @@ object Database {
         return amounts
     }
 
-    //get all the earning sources available within a list
+    /**
+     * Returns the sources of earnings within a specific date
+     *
+     * @param [array] List of earnings within a specific date
+     *
+     * @return all earning sources in list of specific earnings
+     *
+     */
     fun getEarningSources(array: MutableList<Earning>): ArrayList<String>{
         val sources = ArrayList<String>(0)
         var exists = false
@@ -472,7 +622,14 @@ object Database {
         return sources
     }
 
-    //get all the expense sources available within a list
+    /**
+     * Returns expense sources within specific range
+     *
+     * @param [array] List of expenses within specific range
+     *
+     * @return All expense sources within specific range
+     *
+     */
     fun getExpenseSources(array: MutableList<Expense>): ArrayList<String>{
         val sources = ArrayList<String>(0)
         var exists = false
